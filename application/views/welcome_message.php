87241d50b386acbda1a2a60dae289e22c683790f
Udayveer <Udayveernarwaria@gmail.com>
2016-02-20 13:59:08 +0530

    conflict

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<title>Welcome to CodeIgniter uday vijay</title>
+	<title>Welcome to CodeIgniter uday vijay kumar</title>
 
 	<style type="text/css">
 
